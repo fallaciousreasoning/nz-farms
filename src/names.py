d6fad5ee454186270bfc1ed24d15eeb1c35c5b63
Jay Harris <jay.harris@outlook.co.nz>
2020-04-14 21:21:45 +1000

    Begins work on helper methods for working on disk

--- src/names.py
+++ src/names.py
@@ -35,6 +35,31 @@ class Group:
 
         self.titles.append(title.title_no)
 
+connection = None
+def get_root():
+    global connection
+    if connection:
+        return connection.root
+
+    import ZODB
+    connection = ZODB.connection('cache/name_groups')
+    return connection.root
+
+def get_names():
+    if not get_root().names:
+        from BTrees import OOBTree
+        get_root().names = OOBTree.BTree()
+
+    return get_root().names
+
+def get_groups():
+    if not get_root().groups:
+        from BTrees import OOBTree
+        get_root().groups = OOBTree.BTree()
+
+    return get_root().groups
+
+
 def iterate_titles():
     table = DBF('data/titles/nz-property-titles-including-owners-1.dbf')
 
@@ -42,6 +67,7 @@ def iterate_titles():
         yield Title(title_owners(record), record['title_no'])
 
 def build_name_groups():
+    root = get_root()
     for title in iterate_titles():
         last_group = None
         for name in title.names:
@@ -57,4 +83,7 @@ def build_name_groups():
         
         last_group.add_title(title)
 
+
 build_name_groups()
+
+print(len(name_group_map))
