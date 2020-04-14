88509d269d1e2d93ad343a2054b52b3780e20973
Jay Harris <jay.harris@outlook.co.nz>
2020-04-14 11:35:20 +1000

    Take first 10 records

--- src/parse.py
+++ src/parse.py
@@ -1,8 +1,7 @@
 from dbfread import DBF
+import itertools
 
 table = DBF('data/titles/nz-property-titles-including-owners-1.dbf')
-print(table)
 
-for record in table:
-    print(record)
-    break
+for record in itertools.islice(table, 10):
+    print(record['owners'])
