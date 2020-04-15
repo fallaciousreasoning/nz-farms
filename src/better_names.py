a727c2c20c5de4b33e6f649f6501c0074618c96c
Jay Harris <jay.harris@outlook.co.nz>
2020-04-15 18:38:50 +1000

    Tweaks to be a better citizen

--- src/better_names.py
+++ src/better_names.py
@@ -1,6 +1,7 @@
 import typing
 import uuid
 import itertools
+import os
 
 from names import iterate_titles, Title
 
@@ -33,50 +34,41 @@ class Group:
         self.titles.update(group.titles)
         del group_map[group.id]
 
-titles = iterate_titles(lambda p: print(f'\r{round(p*100,2)}%', end=""))
-for title in itertools.islice(titles, None):
-    last_group: Group = None
-    for name in title.names:
-        group = group_map[names_map[name]] if name in names_map else None
-        if last_group and group:
-            last_group.merge_in(group)
-        else: last_group = group
+database_name = 'cache/farms.csv'
+def maybe_build_farm_database():
+    if os.path.exists(database_name):
+        return
+    titles = iterate_titles(lambda p: print(f'\r{round(p*100,2)}%', end=""))
+    for title in itertools.islice(titles, None):
+        last_group: Group = None
+        for name in title.names:
+            group = group_map[names_map[name]] if name in names_map else None
+            if last_group and group:
+                last_group.merge_in(group)
+            else: last_group = group
 
-    if not last_group:
-        last_group = Group()
+        if not last_group:
+            last_group = Group()
 
-    last_group.add_title(title)
+        last_group.add_title(title)
 
-merged_groups = 0
-most_titles = 0
-total_groups = len(group_map)
-total_titles = 0
+    del names_map
+    rows = []
+    for key in group_map.keys():
+        group = group_map[key]
+        for title in group.titles:
+            rows.append((title.row, title.title_no, group.id))
+        del group.names
+        del group.titles
+    del group_map
 
-for group in group_map.values():
-    num_titles = len(group.titles)
-    if num_titles > 1:
-        merged_groups += 1
-    most_titles = max(num_titles, most_titles)
-    total_titles += num_titles
+    rows.sort()
 
-print()
-# 130510 123723 616560 1133494
-print(merged_groups, most_titles, total_groups, total_titles)
+    with open(database_name, mode='w') as f:
+        f.write('row, title_no, group_id\n')
 
-del names_map
-rows = []
-for key in group_map.keys():
-    group = group_map[key]
-    for title in group.titles:
-        rows.append((title.row, title.title_no, group.id))
-    del group.names
-    del group.titles
-del group_map
+        for row, title_no, group_id in rows:
+            f.write(f'{row},{title_no},{group_id}\n')
 
-rows.sort()
-
-with open('cache/farms.csv', mode='w') as f:
-    f.write('row, title_no, group_id\n')
-
-    for row, title_no, group_id in rows:
-        f.write(f'{row},{title_no},{group_id}\n')
+def get_farm_database():
+    maybe_build_farm_database()
