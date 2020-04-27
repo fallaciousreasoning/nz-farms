37972b89f7fc2f9e364cc093061945724c1cfc8b
Jay Harris <jay.harris@outlook.co.nz>
2020-04-27 12:32:42 +1000

    Improves naming

--- src/extract_owners.py
+++ src/extract_owners.py
@@ -1,4 +1,5 @@
 import os
+import itertools
 
 from titles import iterate_titles
 from title_owners import title_owners, name_is_company
@@ -6,18 +7,28 @@ from companies import get_company
 
 def iterate_names():
     for title in iterate_titles():
+        seen_owners = set()
+        owners = []
+        def add_owner(owner, is_company, is_last=False):
+            if owner in seen_owners:
+                return
+
+            seen_owners.add(owner)
+            owners.append((title.record.id, owner, is_last, is_company))
+
+            if not is_last and not name_is_company(owner):
+                parts = owner.split(' ')
+                if len(parts) >= 1:
+                    add_owner(parts[-1], is_company, True)
+
         record = title.record
         names = record.owners.upper().split(', ')
 
         for name in names:
             parts = name.split(' ')
 
-            # Return the name, as it is
-            yield record.id, name, False, False
-
-            # Return the last name
-            if len(parts) >= 1 and not name_is_company(name):
-                yield record.id, parts[-1], True, False
+            # Add the name
+            add_owner(name, False, False)
 
             if name_is_company(name):
                 company_names = get_company(name)
@@ -26,12 +37,6 @@ def iterate_names():
 
                 for director in company_names:
                     director = director.upper()
+                    add_owner(director, True)
 
-                    yield record.id, director, False, True
-
-                    parts = director.split(' ')
-                    # Return the last name
-                    if len(parts) >= 1:
-                        yield record.id, parts[-1], True, True
-
-
+        yield from owners
