09ed5e2bfc70d3316bbd636fd938e06fc28d99e1
Jay Harris <jay.harris@outlook.co.nz>
2020-04-27 15:50:33 +1000

    Use both sets of title data, hopefully

--- src/titles.py
+++ src/titles.py
@@ -1,32 +1,43 @@
 import shapefile
 import typing
 
-input_file = "data/titles/nz-property-titles-including-owners-1"
+input_file_1 = "data/titles/nz-property-titles-including-owners-1"
+input_file_2 = "data/titles/nz-property-titles-including-owners-2"
 
-sf = shapefile.Reader(input_file)
+sf1 = shapefile.Reader(input_file_1)
+sf2 = shapefile.Reader(input_file_2)
 
 
 def get_shape(for_record: int):
-    return sf.shape(for_record)
+    if for_record < len(sf1):
+        return sf1.shape(for_record)
+
+    return sf2.shape(for_record - len(sf1))
 
 
 def get_record(for_record: int):
-    return sf.record(for_record)
+    if for_record < len(sf1):
+        return sf1.record(for_record)
+
+    return sf2.record(for_record - len(sf1))
 
 
 def iterate_titles() -> typing.Iterator[shapefile.ShapeRecord]:
-    for i in range(len(sf)):
-        yield sf.shapeRecord(i)
+    for i in range(len(sf1)):
+        yield sf1.shapeRecord(i)
+
+    for i in range(len(sf2)):
+        yield sf2.shapeRecord(i)
 
 
 def num_titles() -> int:
-    return sf.numRecords
+    return sf1.numRecords + sf2.numRecords
 
 
 def get_title_with_group_writer(filename: str):
     w = shapefile.Writer(filename)
 
-    for field in sf.fields:
+    for field in sf1.fields:
         w.field(*field)
 
     w.field('farm_id', 'N')
